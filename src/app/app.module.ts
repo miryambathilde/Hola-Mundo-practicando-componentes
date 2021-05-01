3a80cb49b49ceef972eee3aed66f120561a89c2a
miryambathilde <mbathilde@gmail.com>
2021-05-01 13:50:28 +0200

    creamos componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 /* IMPORT DEL COMPONENTE DASHBOARD */
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     /* COMPONENTE DASHBOARD */
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
