621a05c9b912d7ac6878bd32276d4d6a89cf9ab9
miryambathilde <mbathilde@gmail.com>
2021-05-01 13:45:43 +0200

    cambiamos vista app html con el la vista dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+/* IMPORT DEL COMPONENTE DASHBOARD */
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    /* COMPONENTE DASHBOARD */
     DashboardComponent
   ],
   imports: [
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /* SELECTOR: nombre del componente app+nombre componente  */
   selector: 'app-dashboard',
+  /* TEMPLATE: HTML */
   templateUrl: './dashboard.component.html',
+  /* STYLE URL: ARCHIVO CSS */
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
